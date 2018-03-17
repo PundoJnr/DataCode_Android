40c67d84f01f311470bf0baf27f4a170fd62c0e4
ArthurPundo <pundoarthur@gmail.com>
2018-03-17 21:05:18 +0300

    final commit

--- app/src/main/java/com/example/poline/data_code/Barcode.java
+++ app/src/main/java/com/example/poline/data_code/Barcode.java
@@ -2,17 +2,25 @@ package com.example.poline.data_code;
 
 import android.*;
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class Barcode extends AppCompatActivity {
@@ -52,11 +60,34 @@ public class Barcode extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //load image
+                Intent intent = new Intent(Intent.ACTION_PICK , MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+
 
             }
         });
 
         }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null !=data){
+            Uri selectImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectImage,filePathColumn,null,null,null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+            imageView.setImageBitmap(bitmap);
+
+            //load data
+            processData(bitmap);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -66,9 +97,18 @@ public class Barcode extends AppCompatActivity {
                     Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
             }
         }
+    }
+    //process barcode
+    private void processData(Bitmap myBitmap){
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<com.google.android.gms.vision.barcode.Barcode> barcodes = detector.detect(frame);
+
+        com.google.android.gms.vision.barcode.Barcode thisCode = barcodes.valueAt(0);
+        textView.setText(thisCode.rawValue);
     }
 }
 
