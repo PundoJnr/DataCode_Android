51758d7ab8f7b271fdc428ddec5a84a72940c9d8
ArthurPundo <pundoarthur@gmail.com>
2018-03-17 19:24:10 +0300

    initial commit

---
+++ app/src/main/java/com/example/poline/data_code/Splash.java
@@ -0,0 +1,13 @@
+package com.example.poline.data_code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
